97d9f29bf311b4a75c65b5275fd1d36d31f8bf36
New-Technologies123 <lka-ufa@yandex.ru>
2025-03-10 16:37:54 +0500

    new

--- src/components/content/contact/Contact.tsx
+++ src/components/content/contact/Contact.tsx
@@ -14,10 +14,10 @@ export const Contact = () => {
         <p>
           <span id="contacts">{t('Информация')}</span>
         </p>
-        <figure className={Styles.leftContent}>
-          <iframe
-            src="https://yandex.ru/map-widget/v1/?um=constructor%3Af1bd327c32b7c5c037613d0c1228b955362997bce9338237e5b612e6449e8c86&amp;source=constructor"
-            width="700"
+        <figure className={Styles.leftContent}>          
+          <iframe 
+            src="https://yandex.ru/map-widget/v1/?um=constructor%3Af1bd327c32b7c5c037613d0c1228b955362997bce9338237e5b612e6449e8c86&amp;source=constructor" 
+            width="700" 
             height="450"
             frameBorder="0"
           />
