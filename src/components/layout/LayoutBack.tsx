776127477dcae9d7012eca1b95cd578f086553d2
New-Technologies123 <lka-ufa@yandex.ru>
2025-04-30 12:16:20 +0500

    product title

--- src/components/layout/LayoutBack.tsx
+++ src/components/layout/LayoutBack.tsx
@@ -6,14 +6,16 @@ export const LayoutBack = ({ title, onBack, children }) => {
   return (
     <ScrollTopOnRender>
       <div className={Styles.hero}>
-        <div className={Styles.backButton} onClick={onBack}>
-          <img src={back.src} alt="" className={Styles.backIcon}/>
-        </div>
-        <div className={Styles.heroContent}>
-          <h1>{title}</h1>
-          <p>Инновационные решения для автоматизации газозаправочных узлов</p>
-        </div>
-      </div>      
+        <div className={Styles.heroHeader}>
+          <div className={Styles.backButton} onClick={onBack}>
+            <img src={back.src} alt="" className={Styles.backIcon}/>
+          </div>
+          <div className={Styles.heroContent}>
+            <h1>{title}</h1>
+            <p>Инновационные решения для автоматизации газозаправочных узлов</p>
+          </div>
+        </div>        
+      </div>
 
       <div className={Styles.content}>{children}</div>
     </ScrollTopOnRender>
