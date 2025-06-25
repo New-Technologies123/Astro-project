7036c702f03761706fd95a19b3c4520a0bae3099
New-Technologies123 <lka-ufa@yandex.ru>
2025-06-25 11:59:15 +0500

    changing buttons on photos

--- src/components/ui/big-photo/BigPhoto.tsx
+++ src/components/ui/big-photo/BigPhoto.tsx
@@ -4,9 +4,9 @@ import Styles from './big-photo.module.scss';
 export const BigPhoto = ({ src, onClose }) => {
   return (
     <div className={Styles.modal} onClick={onClose}>
-      <span className={Styles.close} onClick={(e) => { e.stopPropagation(); onClose(); }}>
+      <button className={Styles.close} onClick={(e) => { e.stopPropagation(); onClose(); }}>
         &times;
-      </span>
+      </button>
       <img src={src} alt="" className={Styles.modalContent} />
     </div>
   );
