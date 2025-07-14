453eb80e549be88508985ad36e2a80f461dd10b6
New-Technologies123 <lka-ufa@yandex.ru>
2025-07-14 16:05:31 +0500

    new News

--- src/components/content/news/News.tsx
+++ src/components/content/news/News.tsx
@@ -1,6 +1,7 @@
 import news_1 from '../../../images/news/news_1.webp';
 import news_2 from '../../../images/news/news_2.webp';
 import news_3 from '../../../images/news/news_3.webp';
+import news_4 from '../../../images/news/news_4.webp';
 import Styles from './news.module.scss';
 import { useTranslation } from 'react-i18next';
 import { Gallery } from '../../ui/gallery/Gallery';
@@ -29,6 +30,12 @@ export const News = () => {
       <div className={`${isLoaded ? Styles.loaded : ''}`}>
         <div className={Styles.newsContent}>
           <div className={Styles.newsItem}>
+            <img src={news_4.src} className={Styles.thumbnail} onClick={() => { setPhotoIsOpen(true); }}/>
+            <p>
+              {t('Грамота')}
+            </p>
+          </div>
+          <div className={Styles.newsItem}>
             <Gallery
               photos={[
                 { id: 1, src: news_1.src, alt: '' },
@@ -49,6 +56,7 @@ export const News = () => {
         </div>        
       </div>
       {photoIsOpen && <BigPhoto src={news_3.src} onClose={() => setPhotoIsOpen(false)} />}
+      {photoIsOpen && <BigPhoto src={news_4.src} onClose={() => setPhotoIsOpen(false)} />}
       <BackToTop/>
     </>
     
